d13c10dc952e1e191dbef0537210142e2af93692
TheDoor <arno.dethoor@gmail.com>
2024-03-07 17:01:56 +0100

    wait

--- turtle/chunk_digger.lua
+++ turtle/chunk_digger.lua
@@ -405,10 +405,11 @@ local function startup()
         waitForKey()
         error("Please check your gps system or the turtle's modem")
     end
+    waitForKey()
     print("Calibrating the turtle ...")
     -- Calibrating the orientation of the turtle
     calibrate(currentPosition)
-
+    waitForKey()
     -- Checking if turtle is already configuered
     if fs.exists(configFilename) then
         print("Loading configuration ...")
@@ -420,6 +421,7 @@ local function startup()
     loadConfigFromFile()
     loadLatestPositionFromFile()
     print("moving to last position")
+    waitForKey()
     moveTo(currentPosition, lastPosition)
 end
 
